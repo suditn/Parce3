797c28a4cf28798f4f23b66bfd4a46934f4fd1be
suditn <43325914+suditn@users.noreply.github.com>
2024-06-16 20:01:30 +0300

    изменения

--- csv_datasheet.py
+++ csv_datasheet.py
@@ -6,60 +6,62 @@ import numpy as np
 from concurrent.futures import ThreadPoolExecutor
 
 # Путь к директории с исходными CSV файлами
+#input_directory = 'exclandcsv/mosfets'
 sl_use = []
 save_path = str(Path(__file__).parent.resolve())
 
 def process_csv_file(input_csv_path, input_directory):
-    try:
-        # Чтение CSV файла
-        df = pd.read_csv(input_csv_path, delimiter=';')
+    # Чтение CSV файла
+    df = pd.read_csv(input_csv_path, delimiter=';')
 
-        # Проверка наличия первого столбца
-        first_column = df.columns[0]
+    # Проверка наличия первого столбца
+    first_column = df.columns[0]  # Получаем имя первого столбца
 
-        # Словарь для хранения информации о каждой серии в текущем CSV файле
-        file_info_local = {}
+    # Словарь для хранения информации о каждой серии в текущем CSV файле
+    file_info_local = {}
 
-        # Создание файлов CSV в соответствующих директориях
-        for series in df[first_column].unique():
-            # Фильтрация данных для текущей серии
-            series_data = df[df[first_column] == series]
+    # Создание файлов CSV в соответствующих директориях
+    for series in df[first_column].unique():
+        # Фильтрация данных для текущей серии
+        series_data = df[df[first_column] == series]
 
-            # Получение пути к директории из столбца "Datasheet part"
-            datasheet_path = series_data['Datasheet part'].iloc[0].replace('\\', '/').replace('//', '/')
-            directory_path = os.path.join(save_path, "Datasheet", input_directory.split("/")[1], series)
+        # Получение пути к директории из столбца "Datasheet part"
+        datasheet_path = series_data['Datasheet part'].iloc[0].replace('\\', '/').replace('//', '/')
+        directory_path = os.path.join(save_path, "Datasheet", input_directory.split("/")[1], series)
 
-            # Путь для сохранения файла CSV
-            output_csv_path = os.path.join(directory_path, f'{series}.csv')
+        # Путь для сохранения файла CSV
+        output_csv_path = os.path.join(directory_path, f'{series}.csv')
 
-            # Проверка, существует ли директория
-            if not os.path.exists(directory_path):
-                os.makedirs(directory_path)
-                print(f"Created directory for series {series}")
+        # Проверка, существует ли директория
+        if not os.path.exists(directory_path):
+            print(datasheet_path)
+            os.makedirs(directory_path)
+            print(f"Created directory for series {series}")
 
-            # Сохранение данных в CSV файл
-            series_data.to_csv(output_csv_path, index=False, sep=';')
+        # Сохранение данных в CSV файл
+        series_data.to_csv(output_csv_path, index=False, sep=';')
 
-            # Формирование путей для PDF и изображения
-            img_path = series_data['Image path'].iloc[0]
-            if isinstance(img_path, np.int64):
-                img_path = None
+        # Формирование путей для PDF и изображения
+        #pdf_path = f"/Datasheet/{input_directory.split('/')[1]}/{series}.pdf"
+        img_path = series_data['Image path'].iloc[0]
+        if type(img_path) is np.int64:
+            img_path = None
 
-            # Добавление информации в локальную JSON структуру для текущего CSV файла
-            file_info_local[series + ".csv"] = {
+        # Добавление информации в локальную JSON структуру для текущего CSV файла
+        file_info_local = {
+            series+".csv":{
                 "pdf": datasheet_path,
                 "img": img_path
             }
+        }
+        print(img_path)
 
-            # Путь для сохранения локального JSON файла рядом с текущим CSV файлом
-            json_local_output_path = os.path.splitext(output_csv_path)[0] + '.json'
+        # Путь для сохранения локального JSON файла рядом с текущим CSV файлом
+        json_local_output_path = os.path.splitext(output_csv_path)[0] + '.json'
 
-            # Запись локального JSON файла
-            with open(json_local_output_path, 'w') as json_file:
-                json.dump(file_info_local, json_file, indent=4)
-        print(f"Processed {input_csv_path}")
-    except Exception as e:
-        print(f"Error processing {input_csv_path}: {e}")
+        # Запись локального JSON файла
+        with open(json_local_output_path, 'w') as json_file:
+            json.dump(file_info_local, json_file, indent=4)
 
 # Использование многопоточности для обработки CSV файлов
 with ThreadPoolExecutor(max_workers=4) as executor:
@@ -68,26 +70,29 @@ with ThreadPoolExecutor(max_workers=4) as executor:
     inputer = input()
     with open('csv_datasheet_p.json', 'r') as f:
         pages = json.load(f)
-
     if inputer == 'all':
         for pa in pages.keys():
             sl_use.append(pages[pa])
+        print(sl_use)
     else:
         for use_in in inputer.split(','):
             if use_in in pages:
+                print(f"Parsing category: {use_in}")
                 for pa in pages[use_in]:
                     sl_use.append(pa)
             else:
                 print(f"Category {use_in} not found in pages.json")
-
+        print(sl_use)
     if inputer == 'all':
         for sl1 in sl_use:
             for sl in sl1:
+                print (sl)
                 for input_csv_path in Path(sl).glob('*.csv'):
                     input_directory = sl
                     futures.append(executor.submit(process_csv_file, input_csv_path, input_directory))
     else:
         for sl in sl_use:
+            print(sl)
             for input_csv_path in Path(sl).glob('*.csv'):
                 input_directory = sl
                 futures.append(executor.submit(process_csv_file, input_csv_path, input_directory))
